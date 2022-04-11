4a03695a441c9e5172c9b4f4d5e530e1fe76d648
Zoffyn <94586498+Zoffyn@users.noreply.github.com>
2022-04-11 22:02:05 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import parseran
 import os
+import time
 
 data_user = []
 data_game = []
@@ -10,19 +11,26 @@ data_kepemilikan = []
 
 directory = ''
 
-def load(path_to_folder):
-    if os.path.isdir(path_to_folder):
-        data_game = parseran.csv_to_matrix(path_to_folder + '/game.csv')
-        data_user = parseran.csv_to_matrix(path_to_folder + '/user.csv')
-        data_riwayat = parseran.csv_to_matrix(path_to_folder + '/riwayat.csv')
-        data_kepemilikan = parseran.csv_to_matrix(path_to_folder + '/kepemilikan.csv')
-
-        return data_game, data_user, data_riwayat, data_kepemilikan
+def load(directory, data_game, data_user, data_riwayat, data_kepemilikan):
+    if os.path.isdir(directory):
+        for i in range(4):
+            print('Loading' + '.' * i, end='\r')
+            time.sleep(0.5)
+        print()
+        print('\nSelamat datang di antarmuka "Binomo"\n')
+        data_game = parseran.csv_to_matrix(directory + '/game.csv')
+        data_user = parseran.csv_to_matrix(directory + '/user.csv')
+        data_riwayat = parseran.csv_to_matrix(directory + '/riwayat.csv')
+        data_kepemilikan = parseran.csv_to_matrix(directory + '/kepemilikan.csv')
+        return True
     else:
-        ...
+        print(f'Folder "{directory}" tidak ditemukan.')
+        return False
 
-def save(path_to_folder):
+def save(directory):
     ...
 
 def exit(save_change):
     ...
+
+load('Data', data_game=data_game, data_user=data_user, data_riwayat=data_riwayat, data_kepemilikan=data_kepemilikan)
