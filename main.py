501cef7750aa80c89442bbf728392a0782f5b03e
Zoffyn <94586498+Zoffyn@users.noreply.github.com>
2022-04-13 22:15:56 +0700

    update main.py

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import argparse
 import parseran
 import os
 import time
+import operasi_array as arr
 
 data_user = []
+data_user_baru = []
 data_game = []
+data_game_baru = []
 data_riwayat = []
 data_riwayat_baru = []
 data_kepemilikan = []
+data_kepemilikan_baru = []
 
-directory = ''
-
-def load(directory, data_game, data_user, data_riwayat, data_kepemilikan):
+def load(directory):
     if os.path.isdir(directory):
         for i in range(4):
             print('Loading' + '.' * i, end='\r')
@@ -22,15 +24,29 @@ def load(directory, data_game, data_user, data_riwayat, data_kepemilikan):
         data_user = parseran.csv_to_matrix(directory + '/user.csv')
         data_riwayat = parseran.csv_to_matrix(directory + '/riwayat.csv')
         data_kepemilikan = parseran.csv_to_matrix(directory + '/kepemilikan.csv')
-        return True
+        return (True, data_game, data_user, data_riwayat, data_kepemilikan)
     else:
         print(f'Folder "{directory}" tidak ditemukan.')
-        return False
+        return (False, [], [], [], [])
 
-def save(directory):
+def save(data_game, data_user, data_riwayat, data_kepemilikan):
+    directory = input('Masukkan nama folder penyimpanan: ')
+    if not os.path.isdir(directory):
+        os.mkdir(directory)
+    print()
+    for i in range(4):
+        print('Saving' + '.' * i, end='\r')
+        time.sleep(0.5)
+    parseran.matrix_to_csv(directory + '/game.csv', data_game)
+    parseran.matrix_to_csv(directory + '/user.csv', data_user)
+    parseran.matrix_to_csv(directory + '/riwayat.csv', data_riwayat)
+    parseran.matrix_to_csv(directory + '/kepemilikan.csv', data_kepemilikan)
+    print('\nData telah disimpan pada folder ' + directory + '!')
+
+def exit():
     ...
 
-def exit(save_change):
-    ...
+(sukses, data_game, data_user, data_riwayat, data_kepemilikan) = load('Data')
 
-load('Data', data_game=data_game, data_user=data_user, data_riwayat=data_riwayat, data_kepemilikan=data_kepemilikan)
+if sukses:
+    save(data_game=data_game, data_user=data_user, data_riwayat=data_riwayat, data_kepemilikan=data_kepemilikan)
