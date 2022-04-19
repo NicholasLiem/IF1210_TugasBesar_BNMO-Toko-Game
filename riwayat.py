7b635eacb02a9c7b71a0d62cbcb234145aba9fb1
energizedNachos <16521108@std.stei.itb.ac.id>
2022-04-19 20:43:04 +0700

    Update riwayat.py

--- riwayat.py
+++ riwayat.py
@@ -2,10 +2,10 @@ import operasi_array as arr
 import constant as c
 
 def riwayat(data_riwayat, username):
-    if not(arr.found_in_kolom(data_riwayat, c.csvID_riwayat_user_id,username)):
+    if not(arr.found_in_kolom(data_riwayat, c.csvID_riwayat_username,username)):
         print(c.r_notFound)
     else:
-        list_riwayat = arr.all_valid_row(data_riwayat, c.csvID_riwayat_user_id, username)
-        riwayat_tanpa_id = arr.delete_column(list_riwayat, c.csvID_riwayat_user_id)
+        list_riwayat = arr.all_valid_row(data_riwayat, c.csvID_riwayat_username, username)
+        riwayat_tanpa_id = arr.delete_column(list_riwayat, c.csvID_riwayat_username)
         print(c.r_listing)
         arr.cetak_tabel(riwayat_tanpa_id)
