ddebf1d3001ccf6c590e71ba9088dcf0caaf16c5
nuhamyahya <103430288+nuhamyahya@users.noreply.github.com>
2022-04-21 04:24:50 +0700

    youww

--- topup.py
+++ topup.py
@@ -1,6 +1,6 @@
 #Penulis Modul: Nicholas Liem - 16521108
 #Judul Modul: Top-up
-#Tanggal: 19 April 2022
+#Tanggal: 10 April 2022
 
 import operasi_array as arr
 import constant as c
@@ -8,17 +8,21 @@ import constant as c
 def topup(data_user):
     username = input(c.t_username)
     saldo_topup = int(input(c.t_saldo))
+
     if not arr.found_in_kolom(data_user,c.csvID_user_username,username):
         print(f"Username '{username}' tidak ditemukan.")
+
     else:
+
         row_id = arr.find_row_id(data_user,c.csvID_user_username,username)
         nama = data_user[row_id][c.csvID_user_nama]
         saldo_data = int(data_user[row_id][c.csvID_user_saldo])
         new_saldo = saldo_topup + saldo_data
 
+        #Ketika jumlah saldo yang diinput dan yang ada hasilnya negatif, maka tidak bisa diproses
         if new_saldo < 0:
             print(c.t_invalid)
             
-        else: #username ada dan saldo hasil positif
+        else: #Username ada dan saldo hasil positif
             data_user[row_id][c.csvID_user_saldo] = str(new_saldo)
             print(f"Top up berhasil. Saldo {nama} bertambah menjadi {new_saldo}.")
--- ubah_stok.py
+++ ubah_stok.py
@@ -1,6 +1,6 @@
 #Penulis Modul: Nicholas Liem - 16521108
 #Judul Modul: Ubah Stok Game
-#Tanggal: 19 April 2022
+#Tanggal: 9 April 2022
 
 import operasi_array as arr
 import constant as c
@@ -32,4 +32,3 @@ def ubah_stok(data_game):
             new_stock_data = stock_data + stock_input
             data_game[row_id][c.csvID_game_stock] = str(new_stock_data)
             print(f"Stok game {nama_game} berhasil dikurangi. Stok sekarang: {new_stock_data}")
-        return data_game
