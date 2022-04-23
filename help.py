5110e11eeca44f3fea826be12b2961d69374c825
energizedNachos <16521108@std.stei.itb.ac.id>
2022-04-23 13:37:55 +0700

    Update help.py

--- help.py
+++ help.py
@@ -9,9 +9,11 @@ def help(role):
         print("2. login - Untuk melakukan login ke dalam sistem")
         print("3. tambah_game - Untuk menambah game yang dijual pada toko")
         print("4. list_game_toko - Untuk melihat list game yang dijual pada toko")
-    else:
+    elif role == "user":
         print("============ HELP ============")
         print("1. login - Untuk melakukan login ke dalam sistem")
         print("2. list_game_toko - Untuk melihat list game yang dijual pada toko")
-
-#belom ada role
+    else:
+        print("============ HELP ============")
+        print("1. login - Untuk melakukan login ke dalam sistem")
+        print("2. help - Untuk melihat perintah yang bisa dilakukan")
