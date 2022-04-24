92c66c31fcb4b7113585fdd8ad9c0639cfd9a618
Zoffyn <94586498+Zoffyn@users.noreply.github.com>
2022-04-24 11:43:00 +0700

    benerin bug di ubah_stok.py

--- ubah_stok.py
+++ ubah_stok.py
@@ -26,14 +26,20 @@ def ubah_stok(data_game):
 
             #Kalau stok inputnya positif, pastinya akan diproses dan ditambahkan
             elif stock_input >= 0:
+                # Menghindari bug
                 new_stock_data = stock_data + stock_input
-                data_game[row_id][c.csvID_game_stock] = str(new_stock_data)
+                temp = arr.copy(data_game[row_id])
+                temp[c.csvID_user_saldo] = str(new_stock_data)
+                data_game[row_id] = temp
                 print(f"Stok game {nama_game} berhasil ditambahkan. Stok sekarang: {new_stock_data}")
             
             #Kasus ketika stok input negatif tetapi jika dijumlahkan dengan stok data hasilnya tetap positif
             else: #(0 < -1*stock_input < stock_data):
+                # Menghindari bug
                 new_stock_data = stock_data + stock_input
-                data_game[row_id][c.csvID_game_stock] = str(new_stock_data)
+                temp = arr.copy(data_game[row_id])
+                temp[c.csvID_user_saldo] = str(new_stock_data)
+                data_game[row_id] = temp
                 print(f"Stok game {nama_game} berhasil dikurangi. Stok sekarang: {new_stock_data}")
         else:
             print("Stok input tidak boleh kosong.")
