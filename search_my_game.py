f549bd5abc2b077eddbda0bcfb5a93ec064ece70
energizedNachos <16521108@std.stei.itb.ac.id>
2022-04-24 09:40:42 +0700

    updates

--- search_my_game.py
+++ search_my_game.py
@@ -14,17 +14,19 @@ import constant as c
 def search_my_game(data_kepemilikan, data_game, username):
     #seach_my_game() menerima input (id_game atau tahun_rilis atau tidak keduanya) 
     #dan mengeluarkan output yang diminta.
+    sliced_header = arr.delete_column(data_game, c.csvID_game_stock)[0]
 
     id_game = input(c.s_idGame)
     tahun_rilis = input(c.s_releaseYear)
     data_kepemilikan_user = arr.all_valid_row(data_kepemilikan, c.csvID_kepemilikan_user_id, username)
 
-    baris_game_user = [data_game[0]]
+    baris_game_user = [sliced_header]
     for item in data_kepemilikan_user:
         row_id = arr.find_row_id(data_game, c.csvID_game_id, item[c.csvID_kepemilikan_game_id])
         baris_game = data_game[row_id]
         baris_game_user = arr.fungsi_append(baris_game_user, baris_game)
-    if baris_game_user == [data_game[0]]:
+
+    if baris_game_user == [sliced_header]:
         print(c.s_notFound)
     else:
         print(c.s_listing)
@@ -35,7 +37,7 @@ def search_my_game(data_kepemilikan, data_game, username):
             #"Dictionary" lokal supaya mempermudah debugging dan lebih enak dilihat
             tahun_rilis_in_array = arr.found_in_kolom(baris_game_user, c.csvID_game_releaseYear, tahun_rilis)
             if tahun_rilis_in_array:
-                data_baris_tahun_rilis = arr.delete_column((arr.all_valid_row(baris_game_user, c.csvID_game_releaseYear, tahun_rilis)), c.csvID_game_stock)
+                data_baris_tahun_rilis = arr.delete_column(arr.all_valid_row(baris_game_user, c.csvID_game_releaseYear, tahun_rilis), c.csvID_game_stock)
             
             id_game_in_array = arr.found_in_kolom(baris_game_user, c.csvID_game_id, id_game)
             if id_game_in_array:
@@ -44,19 +46,26 @@ def search_my_game(data_kepemilikan, data_game, username):
             #Kasus pertama mengecek jika input id_game kosong
             #jika tahun yang dimasukkan merupakan bagian dari data_riwayat, maka akan diprint semua data yang berhubungan dengan tahun tsb
             if (id_game == "") and tahun_rilis_in_array:
-                arr.cetak_tabel(data_baris_tahun_rilis)
+                array_without_header = [sliced_header]
+                array_without_header = arr.fungsi_append(array_without_header, data_baris_tahun_rilis[0])
+                arr.cetak_tabel(array_without_header)
 
             #Kasus kedua mengecek jika tahun yang diisi kosong, dan mengecek jika id_game inputnya terdaftar di data_riwayat, kemudian
             #jika terdaftar di data_riwayat, akan dikeluarkan data sesuai dengan id_game yang diinput
             elif (tahun_rilis == "") and id_game_in_array:
-                arr.cetak_tabel(data_baris_game_id)
+                array_without_header = [sliced_header]
+                array_without_header = arr.fungsi_append(array_without_header, data_baris_game_id[0])
+                arr.cetak_tabel(array_without_header)
 
             #Kasus ketiga mengecek jika id_game yang diinput berada dalam data_riwayat, kemudian mengecek apakah pada baris id_game tersebut
             #tahunnya sama, jika sama maka akan dikeluarkan data berdasarkan id_game tersebut. Mengapa kasus ini dibuat?
             #Kasus ini dibuat untuk kasus di mana kedua input benar (id_game dan tahun_rilis). Kasus-kasus di atas tidak membahas jika kedua
             #variabel terisi, oleh karena itu diperlukan kasus khusus ini.
             elif id_game_in_array and (data_baris_game_id)[0][c.csvID_game_releaseYear] == tahun_rilis:
-                arr.cetak_tabel(data_baris_game_id)
+                array_without_header = [sliced_header]
+                for item in data_baris_game_id:
+                    array_without_header = arr.fungsi_append(array_without_header, item)
+                arr.cetak_tabel(array_without_header)
                 
             #Tidak memenuhi semua kasus, artinya data tidak ditemukan.
             else:
